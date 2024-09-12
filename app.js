2523200c7fcbdfc809c8d09576359d874fee0c6d
ARSHamirali <arshamirali604@gmail.com>
2024-09-12 13:59:50 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,218 @@
+// chapter #5
+// Q1
+// var num1 =8
+// var num2 = 2
+// var result = 8+2
+// document.write("Sum of", num1 ,"and" ,num2,"is",result)
+
+// Q2
+// var num1 =8
+// var num2 = 2
+// var result = 8-2
+// document.write("Sum of", num1 ,"and" ,num2,"is",result)
+
+// Q3
+
+// var num1 = "??"
+// document.write("Value after variable declaration is", num1)
+// num1 = 5;
+// document.write("“Initial value:", num1)
+
+// var result = --num1
+// document.write("“Value after increment is:", result)
+
+// var result2 = result+7
+// document.write("“Value after addition is:", result2)
+
+// var final = result2 / 11
+// document.write("“Final value is:", final)
+
+// Q4
+//  var ticket_price = 600
+//  var total_tickets = 5*ticket_price
+//  document.write("Total cost to buy 5 tickets to a movie is PKR", total_tickets)
+
+// Q5
+// document.write("4 x 1 = 4 <br> 4 x 2 =8 <br> 4 x 3 = 12 <br> .... <br> 4 x 10 = 40")
+
+// Q6
+// var C1 = 45
+// var f = (C1 * 9/5) +5
+// document.write("Temperature in celsius is: ", C1," Temperature in fahrenheit",f)
+// var f =35
+// var C2 =  (f-5)*5/9
+// document.write(  "Temperature in fahrenheit is: ", f," Temperature in celsius",C2)
+
+// Q7
+// var cost1 =900
+// var q1 =5 
+// var cost2 =1200
+// var q2 =8
+// document.write("The price od item one is",cost1)
+// document.write("The qunatity of item one is 1",q1)
+// document.write("The price od item two is",cost2)
+// document.write("The qunatity of item one is 2",q2)
+// var Total= cost1 + cost2
+// document.write("Your total bill is",Total)
+ 
+// Q8
+// var Total_marks=980
+// var MarksObtain =804
+// var percentage = (840/980)*100
+// document.write("Total marks <br>",Total_marks,"<br> Mark obtain <br>",MarksObtain,"<br> Percentage <br>",percentage)
+
+// Q9
+// var US$ = 10
+// var RIYAL = 25
+// var conversion1 = 10*104.80
+// var conversion2 = 25*28
+// var totalpkr = conversion1 + conversion2
+// document.write("Total PKR =",totalpkr)
+
+// Q10
+// var number1=2
+// var Answer =2*10/2
+
+// Q11
+// var BOD = 2005
+// var CURRENTYEAR = 2024
+// var age = CURRENTYEAR-BOD
+// document.write("Current age",CURRENTYEAR,"<br> YEAR OF BIRTH",BOD,"<br> age",age)
+
+// Q12
+// var radius=20
+// var circumference=2*3.142*radius
+// var area=3.142*radius*radius
+// document.write("Radius=",radius,"<br> Circumference=",circumference,"<br> Area=",area)
+
+// Q13
+// var current=19
+// var maxage=50
+// var item="fries"
+// var cost3=80
+// var eatingperday=1
+// var totalanswer=maxage*cost3
+// document.write("Total cost",totalanswer)
+
+
+// Chapter #6
+// Q1
+// var a =10
+// document.write("The value of 'a' is",a)
+  
+// var b=++a
+// document.write("Now the value of 'a' is",b)
+
+// var c=a++
+// document.write("Now the value of 'a' is",a)
+
+// var d=--a
+// document.write("Now the value of 'a' is",d)
+ 
+// var e=a--
+// document.write("Now the value of 'a' is",a)
+
+// Q2
+// var a=2
+// var b=1
+// var c=--a
+// var d=--a - --b
+// var e= --a + --b + ++b
+// var f=--a + --b + ++b + b--
+// document.write(f)
+
+// Q3
+// prompt("ENTER YOUR NAME")
+
+// Q4
+// var numbers1=prompt("Input a number")
+// var numbers2=prompt("Input another number")
+// var numbers3=numbers1*numbers2
+// document.write(numbers3)
+
+// Q5
+// var subject1="Math"
+// var subject2="English"
+// var subject3="Science"
+// document.write(subject1,"<br>",subject2,"<br>",subject3)
+// var Totalmarks=100
+// document.write(Totalmarks,"<br>",Totalmarks,"<br>",Totalmarks)
+// var MarkObtain=94
+// var MarkObtain2=92
+// var MarkObtain3=93
+// var percentage=93/100*100
+// document.write(percentage)
+
+// Q6
+// var X= ++X
+
+// Q7
+// var x= 100
+// var y= --x 
+  
+// Q8
+// var x=50
+// var y= x--
+//  value of y is now 50
+
+// Q9
+// var num1=20
+// var num2=--num1 + ++num1
+// var newnum=num2
+
+//Q10
+// var num1=20
+// var num2=++num1 - --num1
+// var newnum=num2
+
+// Q11
+// var num1=20
+// var num2= ++num1
+// alert(num2)
+
+
+// Chapter #7  
+//Q1
+// var calulatednum=2+(2*6)
+// console.log(calulatednum)
+ 
+//Q2
+// var calulatednum=(2+2)*(4+2)
+// console.log(calulatednum)
+
+// Q3
+// var calulatednum=((2+2)*4)+2 
+// console.log(calulatednum)
+
+
+// Chapter #8
+//Q1
+// var num="2"
+// console.log(num+num)
+
+//Q2
+// var value= "Hello"
+// var value2="Dolly"
+// alert(value+value2)
+
+//Q3
+// alert("33"+3)
+
+//Q4
+//alert("Pakistan"+" "+"Zinabad")
+
+//Q5
+//var value="Arsh"+2006
+
+// Q6
+// var firstname="Arsh"
+// var lastname="Amirali"
+// console.log(firstname+lastname)
+
+// Chapter #9
+//Q1
+//prompt("Enter your firstname")
+
+//Q2
+//prompt("Conutry")
+//document.write("China")
